29157db42cea0ed874e02329bc669f505adb3dff
sidney2k1 <sidsingla02@gmail.com>
2024-10-26 11:10:08 +0530

    main3

---
+++ main3.py
@@ -0,0 +1,5 @@
+countrycode={"India":"0091","Nepal":"0081","Australia":"0025"}
+print("Country code of india:")
+print(countrycode.get("India","Not found"))
+print("Country code for Japan")
+print(countrycode.get("Japan","Not found"))
